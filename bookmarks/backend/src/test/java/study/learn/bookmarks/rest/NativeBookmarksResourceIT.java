5afc99dd981468dc8c12f6e3195d858374e0ca52
Michael Vorburger ⛑️ <mike@vorburger.ch>
2019-04-28 06:41:16 +0000

    mvn io.quarkus:quarkus-maven-plugin:V:create
    mvn io.quarkus:quarkus-maven-plugin:0.14.0:create \
        -DprojectGroupId=study.learn \
        -DprojectArtifactId=bookmarks \
        -DclassName="study.learn.bookmarks.rest.BookmarksResource" \
        -Dpath="/bookmarks" \
        -Dextensions="resteasy-jsonb"

---
+++ bookmarks/backend/src/test/java/study/learn/bookmarks/rest/NativeBookmarksResourceIT.java
@@ -0,0 +1,9 @@
+package study.learn.bookmarks.rest;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeBookmarksResourceIT extends BookmarksResourceTest {
+
+    // Execute the same tests but in native mode.
+}
